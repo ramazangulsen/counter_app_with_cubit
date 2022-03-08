8700f4a3c1d47d97e49b920e41fbfedb8ff5f003
Ramazan Gülşen <97034453+ramazangulsen@users.noreply.github.com>
2022-03-08 20:19:20 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:developer';
 
 import 'package:counter_app_cubit/bloc_counter/counter_bloc.dart';
 
-import 'package:counter_app_cubit/cubit_counter/cubit_counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,15 +14,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-        providers: [
-          BlocProvider(
-            create: (context) => CubitCounterCubit(),
-          ),
-          BlocProvider(
-            create: (context) => CounterBloc(),
-          )
-        ],
+    return BlocProvider(
+        create: (context) => CounterBloc(),
         child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
@@ -31,63 +23,6 @@ class MyApp extends StatelessWidget {
           ),
           home: const MyHomePageWithBloc(),
         ));
-  }
-}
-
-class MyHomePageWithCubit extends StatelessWidget {
-  const MyHomePageWithCubit({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    print("print triggered");
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Counter App With Cubit'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            /*  Text(
-              context.watch<CubitCounterCubit>().state.counter.toString(),
-              style: Theme.of(context).textTheme.headline4,
-            ), 
-            */
-            BlocBuilder<CubitCounterCubit, CubitCounterState>(
-              // bu sayede komple widgeti build etmek yerine değişiklik yapmak istediğimiz yeri build ediyoruz. build üstüne print yazdık. sadece butona ilk bastığımızda tetikleniyor.
-              builder: (context, CubitCounterState state) {
-                return Text(
-                  state.counter.toString(),
-                  style: Theme.of(context).textTheme.headline4,
-                );
-              },
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              context.read<CubitCounterCubit>().increment();
-            },
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              context.read<CubitCounterCubit>().decrement();
-            },
-            tooltip: 'Decrement',
-            child: const Icon(Icons.minimize_rounded),
-          ),
-        ],
-      ),
-    );
   }
 }
 
